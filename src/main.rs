bc2f576156eca1e61dc8944eb40278cfb4ae470f
ovierdev <ovier.obregon@outlook.com>
2026-06-06 22:26:34 +0300

    second commit
    first script for tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{Result, client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address.
+    let mut client = client::connect("127.0.01:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("Got value from the server; result={:.?}", result);
+
+    Ok(())
 }
